df1bfa2ff4be5efaadc2626aa59b50d9592cf571
Roman Marchenko <kirkusik@gmail.com>
2023-07-01 13:49:03 +0300

    single serv ch

--- src/js/modules/more-services.js
+++ src/js/modules/more-services.js
@@ -1,16 +1,33 @@
 export default function createMoreServicesSlider(Swiper) {
-    const moreServicesSlider = document.querySelector(".more-services__slider");
-    if (moreServicesSlider) {
-        const swiper = new Swiper(moreServicesSlider, {
-            grabCursor: true,
-            slidesPerView: 3,
-            spaceBetween: 48,
-            autoplay: true,
-            pagination: {
-                el: ".swiper-pagination",
-                type: "bullets",
-                clickable: true,
-            },
-        });
-    }
+	const moreServicesSlider = document.querySelector(".more-services__slider");
+	if (moreServicesSlider) {
+		const swiper = new Swiper(moreServicesSlider, {
+			grabCursor: true,
+			slidesPerView: 3,
+			spaceBetween: 48,
+			autoplay: true,
+
+			breakpoints: {
+				320: {
+					spaceBetween: 20,
+					slidesPerView: 1,
+				},
+
+				992: {
+					spaceBetween: 30,
+					slidesPerView: 2,
+				},
+				1200: {
+					spaceBetween: 40,
+					slidesPerView: 3,
+				},
+			},
+
+			pagination: {
+				el: ".swiper-pagination",
+				type: "bullets",
+				clickable: true,
+			},
+		});
+	}
 }
