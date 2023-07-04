c9916b15a5927013bbfef5d92adac88a9f94bb02
Roman Marchenko <kirkusik@gmail.com>
2023-07-04 15:19:54 +0300

    Initial dist subtree commit

--- src/js/modules/menu-handler.js
+++
@@ -1,9 +0,0 @@
-export default function menuHandler() {
-	const menuToogleBtn = document.querySelector(".menu-toggle");
-	const pageNav = document.querySelector(".page-header__nav");
-
-	menuToogleBtn.addEventListener("click", () => {
-		pageNav.classList.toggle("page-header__nav--active");
-		document.body.classList.toggle("page__body--no-scroll");
-	});
-}
